e8ddb21ca4d63521d6bef4f7b49f3c22a1d29f9b
alleksandrb <bashtein@gmail.com>
2025-09-09 01:11:18 +0200

    rename session key

--- app/Services/QueueService.php
+++ app/Services/QueueService.php
@@ -14,7 +14,7 @@ class QueueService
     public function addViewIncrementTask(TelegramAccount $account, string $telegramPostUrl): void
     {
         $jsonPayload = json_encode([
-            'account_id' => base64_encode($account->getSession()),
+            'session' => $account->getSession(),
             'account_json_data' => $account->getJsonData(),
             'proxy' => $account->proxy()->first()->toArray(),
             'telegram_post_url' => $telegramPostUrl
